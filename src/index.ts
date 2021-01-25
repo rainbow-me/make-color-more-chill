2f2a3176a717293e755b1a38d4de037ffae85f47
Michael Demarais <m.demarais@gmail.com>
2021-01-25 02:02:28 -0500

    support for custom background color used when comparing contrast values

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,41 @@ import chroma from 'chroma-js';
 const chillThreshold = 2.5;
 
 export const black = '#000000';
-export const dark = '#25292E';
 export const white = '#ffffff';
+
+export const fallbackColors = {
+  dark: '#25292E',
+  light: '#525B66',
+};
 
 export function isBlackOrWhite(color: string): boolean {
   return chroma(color).hex() === black || chroma(color).hex() === white;
 }
 
-export function isChill(color: string): boolean {
-  const chillness = Number(chroma.contrast(color, white).toFixed(2));
+export function isChill(color: string, background: string = white): boolean {
+  const chillness = Number(chroma.contrast(color, background).toFixed(2));
   return chillness > chillThreshold;
 }
 
-export default function makeColorMoreChill(color: string) {
+export function fallbackColorForBackground(background: string = white) {
+  return chroma.distance(white, background) < chroma.distance(black, background)
+    ? fallbackColors.dark
+    : fallbackColors.light;
+}
+
+export default function makeColorMoreChill(
+  color: string,
+  background: string = white
+) {
   // Pure white and pure black do not chill well with our interfaces, lets make it more chill.
-  if (isBlackOrWhite(color)) return dark;
+  if (isBlackOrWhite(color)) return fallbackColorForBackground(background);
 
   // Return the color if it's already totally chill and doesn't need to be messed with.
-  if (isChill(color)) return color;
+  if (isChill(color, background)) return color;
 
   // Mess with the color just enough to make it pass the chillThreshold, but not too far past it.
   let chillColor = color;
-  while (!isChill(chillColor)) {
+  while (!isChill(chillColor, background)) {
     chillColor = chroma(chillColor)
       .darken(0.02)
       .saturate(0.02)
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,11 @@
-import makeColorMoreChill, { black, dark, isChill, white } from '../src';
+import makeColorMoreChill, {
+  black,
+  fallbackColors,
+  isChill,
+  white,
+} from '../src';
 
+const { dark, light } = fallbackColors;
 const chillColor = '#3F6AFF';
 const unchillColor = '#F0E7EA';
 
@@ -13,7 +19,16 @@ describe('makeColorMoreChill', () => {
   it('returns "dark" color instead of pure white', () => {
     expect(makeColorMoreChill(white)).toEqual(dark);
   });
+  it('returns "light" color instead of pure black when given a dark background', () => {
+    expect(makeColorMoreChill(black, dark)).toEqual(light);
+  });
+  it('returns "light" color instead of pure white when given a dark background', () => {
+    expect(makeColorMoreChill(white, dark)).toEqual(light);
+  });
   it('returns a chill color when provided an unchill color', () => {
     expect(isChill(makeColorMoreChill(unchillColor))).toEqual(true);
+  });
+  it('returns a chill color when provided an unchill color against dark background', () => {
+    expect(isChill(makeColorMoreChill(unchillColor, dark), dark)).toEqual(true);
   });
 });
